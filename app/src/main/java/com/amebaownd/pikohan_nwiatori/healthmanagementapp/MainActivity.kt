cd8bae7207ada568fdfa723ec4b7625fabd76161
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-04 01:15:04 +0900

    add navigation setup

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/MainActivity.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/MainActivity.kt
@@ -5,35 +5,53 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var drawerLayout :DrawerLayout
+    private lateinit var appBarConfiguration:AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        setupDrawerLayout()
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        val navController :NavController = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration =
+            AppBarConfiguration.Builder(R.id.todayFragment)
+                .setDrawerLayout(drawerLayout)
+                .build()
+        setupActionBarWithNavController(navController,appBarConfiguration)
+        nav_view.setupWithNavController(navController)
+    }
+
+    override fun onBackPressed() {
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
+    private fun setupDrawerLayout(){
+        drawerLayout = findViewById(R.id.drawer_layout)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp(appBarConfiguration)
+                || super.onSupportNavigateUp()
     }
+
 }

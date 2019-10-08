6c88f04643f976f0a1f8f697b7462dc5e4f80bd0
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-08 23:42:48 +0900

    add application

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/util/HealthManagementApp.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/util/HealthManagementApp.kt
@@ -2,16 +2,37 @@ package com.amebaownd.pikohan_nwiatori.healthmanagementapp.util
 
 
 import android.app.Application
+import android.content.Context
+import androidx.test.runner.lifecycle.ApplicationLifecycleCallback
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.ServiceLoader
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.data.repository.FoodStuffRepository
+import java.lang.RuntimeException
 
 class HealthManagementApp : Application() {
     val repository: FoodStuffRepository
         get() = ServiceLoader.provideFoodStuffRepository(
             this
         )
-
     override fun onCreate() {
         super.onCreate()
+        MyContext.onCreateAppcalition(this)
+    }
+}
+
+class MyContext(application :Context){
+    var applicationContext = application
+    companion object{
+        private lateinit var instance:MyContext
+        fun onCreateAppcalition(applicationContext:Context){
+            instance = MyContext(applicationContext)
+        }
+        fun getContext():Context{
+            if(instance.applicationContext==null)
+                throw RuntimeException()
+            return instance.applicationContext
+        }
+        fun getString(resId:Int):String{
+            return instance.applicationContext.getString(resId)
+        }
     }
 }

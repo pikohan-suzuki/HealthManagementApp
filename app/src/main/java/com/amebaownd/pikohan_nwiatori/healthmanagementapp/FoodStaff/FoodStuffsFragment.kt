9d66b740c7a47656e8adec1cb701fff8566380d1
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-11 23:09:39 +0900

    add_serch_function

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/FoodStaff/FoodStuffsFragment.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/FoodStaff/FoodStuffsFragment.kt
@@ -2,20 +2,23 @@ package com.amebaownd.pikohan_nwiatori.healthmanagementapp.foodstaff
 
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.fragment.app.viewModels
-import com.amebaownd.pikohan_nwiatori.healthmanagementapp.EventObserver
+import androidx.navigation.fragment.navArgs
+import com.amebaownd.pikohan_nwiatori.healthmanagementapp.util.EventObserver
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.R
+import com.amebaownd.pikohan_nwiatori.healthmanagementapp.data.model.FoodStuff
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.databinding.FragmentFoodstuffsBinding
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.util.getViewModelFactory
 import kotlinx.android.synthetic.main.fragment_foodstuffs.*
 
-class FoodStuffsFragment : Fragment() {
+class FoodStuffsFragment : Fragment(){
 
     private val foodStuffViewModel: FoodStuffViewModel by viewModels<FoodStuffViewModel> { getViewModelFactory() }
+    private val args:FoodStuffsFragmentArgs by navArgs()
 
     private lateinit var viewDataBinding: FragmentFoodstuffsBinding
     private lateinit var listAdapter: FoodStuffsAdapter
@@ -37,18 +40,38 @@ class FoodStuffsFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
         setupRecyclerView()
         setupFub()
+        foodStuffViewModel.start(args.isAddEditDish)
         openDetail()
+        selectFoodStuff()
+        onSearchTextChanged()
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_sort ->{
+
+                true
+            }
+            R.id.filter_food_group1->{
+
+                true
+            }
+            R.id.filter_food_group2->{
+
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
     private fun openFilterPopUpMenu() {
-
+//        val view = activity?.findViewById<View>(R.id.action_filter)?:return
+//        PopupMenu(requireContext(),view).run {
+//            menuInflater.inflate(R.menu.sort_menu,menu)
+//            setOnMenuItemClickListener {
+//
+//            }
+//        }
     }
 
     private fun setupFub() {
@@ -57,6 +80,19 @@ class FoodStuffsFragment : Fragment() {
 //                .setAction("Action", null).show()
             navigateToAddFoodstuffFragment()
         }
+    }
+    
+    private fun onSearchTextChanged(){
+        viewDataBinding.foodstuffsSearch.setOnQueryTextListener(object:SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                newText?.let {foodStuffViewModel.filterItems(it)  }
+                return true
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -73,13 +109,32 @@ class FoodStuffsFragment : Fragment() {
         findNavController().navigate(action)
     }
 
+    private fun navigateToAddEditDishFragment(foodStuff: FoodStuff){
+        val action=
+            FoodStuffsFragmentDirections
+                .actionFoodStuffsFragmentToAddEditDishFragment(
+                    dishId = null,
+                    title = resources.getString(R.string.add_dish_title),
+                    foodstuff = foodStuff
+                )
+        findNavController().navigate(action)
+    }
+
     private fun openDetail() {
-        foodStuffViewModel.openDetailEvent.observe(this, EventObserver {
-            if(it.isNotBlank() && it.isNotEmpty()) {
-                val action =
-                    FoodStuffsFragmentDirections
-                        .actionFoodStuffsToDetailFoodStuffFragment(it)
-                findNavController().navigate(action)
+        foodStuffViewModel.openDetailEvent.observe(this,
+            EventObserver {
+                if (it.isNotBlank() && it.isNotEmpty()) {
+                    val action =
+                        FoodStuffsFragmentDirections
+                            .actionFoodStuffsToDetailFoodStuffFragment(it)
+                    findNavController().navigate(action)
+                }
+            })
+    }
+    private fun selectFoodStuff(){
+        foodStuffViewModel.selectFoodStuffEvent.observe(this,EventObserver{
+            it.let{
+                navigateToAddEditDishFragment(it)
             }
         })
     }

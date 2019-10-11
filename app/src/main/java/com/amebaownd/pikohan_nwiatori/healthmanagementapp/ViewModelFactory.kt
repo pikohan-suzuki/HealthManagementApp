814c93527db06841db8d583c71fca03962dcba49
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-11 23:07:27 +0900

    viewModelFactory

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/ViewModelFactory.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/ViewModelFactory.kt
@@ -2,7 +2,11 @@ package com.amebaownd.pikohan_nwiatori.healthmanagementapp
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.amebaownd.pikohan_nwiatori.healthmanagementapp.data.repository.DishRepository
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.data.repository.FoodStuffRepository
+import com.amebaownd.pikohan_nwiatori.healthmanagementapp.dish.DishesViewModel
+import com.amebaownd.pikohan_nwiatori.healthmanagementapp.dish.addEditDish.AddEditDishViewModel
+import com.amebaownd.pikohan_nwiatori.healthmanagementapp.dish.detailDish.DetailDishViewModel
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.foodstaff.FoodStuffViewModel
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.foodstaff.addEditFoodStuff.AddEditFoodStuffViewModel
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.foodstaff.detailFoodStuff.DetailFoodStuffViewModel
@@ -10,7 +14,8 @@ import java.lang.IllegalArgumentException
 
 @Suppress("UNCHECKED_CAST")
 class ViewModelFactory (
-    private val foodStuffRepository: FoodStuffRepository
+    private val foodStuffRepository: FoodStuffRepository,
+    private val dishRepository: DishRepository
 ):ViewModelProvider.NewInstanceFactory(){
 
     override fun <T: ViewModel> create(modelClass: Class<T>)=
@@ -22,6 +27,12 @@ class ViewModelFactory (
                     AddEditFoodStuffViewModel(foodStuffRepository)
                 isAssignableFrom(DetailFoodStuffViewModel::class.java)->
                     DetailFoodStuffViewModel(foodStuffRepository)
+                isAssignableFrom(DishesViewModel::class.java)->
+                    DishesViewModel(dishRepository)
+                isAssignableFrom(AddEditDishViewModel::class.java)->
+                    AddEditDishViewModel(dishRepository)
+                isAssignableFrom(DetailDishViewModel::class.java)->
+                    DetailDishViewModel(dishRepository)
                 else->
                     throw IllegalArgumentException("Unknown ViewModelclass ${modelClass}")
             }

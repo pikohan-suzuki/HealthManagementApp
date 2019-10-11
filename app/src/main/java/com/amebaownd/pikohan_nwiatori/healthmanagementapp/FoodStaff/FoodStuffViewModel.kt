1f9f47b720c8ef23772456121f674269d818b36a
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-11 23:09:22 +0900

    foodStuffViewModel

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/FoodStaff/FoodStuffViewModel.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/FoodStaff/FoodStuffViewModel.kt
@@ -1,32 +1,49 @@
 package com.amebaownd.pikohan_nwiatori.healthmanagementapp.foodstaff
 
-import android.app.Application
 import androidx.lifecycle.*
-import com.amebaownd.pikohan_nwiatori.healthmanagementapp.Event
-import com.amebaownd.pikohan_nwiatori.healthmanagementapp.data.HealthDatabase
+import com.amebaownd.pikohan_nwiatori.healthmanagementapp.util.Event
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.data.model.FoodStuff
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.data.repository.FoodStuffRepository
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
-import kotlin.coroutines.CoroutineContext
 
 class FoodStuffViewModel (private val repository: FoodStuffRepository):ViewModel(){
-    val allFoodStuffs : LiveData<List<FoodStuff>> = repository.allFoodStuffs
+    private var _allFoodStuffs = MutableLiveData<List<FoodStuff>>()
+    val allFoodStuffs : LiveData<List<FoodStuff>> = _allFoodStuffs
 
     private val _openDetailEvent = MutableLiveData<Event<String>>()
     val openDetailEvent:LiveData<Event<String>> = _openDetailEvent
 
+    private val _isSelectMode = MutableLiveData<Boolean>(false)
+    val isSelectMode :LiveData<Boolean> = _isSelectMode
+
+    private val _selectFoodStuffEvent = MutableLiveData<Event<FoodStuff>>(null)
+    val selectFoodStuffEvent :LiveData<Event<FoodStuff>> = _selectFoodStuffEvent
+
     init{
-        _openDetailEvent.value=Event("")
-    }
-    fun openDetail(taskId:String){
-        _openDetailEvent.value = Event(taskId)
+        _openDetailEvent.value= Event("")
     }
 
-    fun insert(foodstuff:FoodStuff) = viewModelScope.launch {
-        repository.insert(foodstuff)
+    fun start(isSelectMode:Boolean){
+        if(isSelectMode)
+            this._isSelectMode.value=true
+
+        viewModelScope.launch(Dispatchers.IO){
+            _allFoodStuffs.postValue(repository.loadAllFoodStuffs())
+        }
+    }
+
+    fun filterItems(key:String){
+        viewModelScope.launch(Dispatchers.IO){
+            _allFoodStuffs.postValue(repository.loadFiltered(key))
+        }
+    }
+
+    fun onClick(foodStuff: FoodStuff){
+        if(isSelectMode.value!!)
+            _selectFoodStuffEvent.value=Event(foodStuff)
+        else
+            _openDetailEvent.value = Event(foodStuff.id)
     }
 
 }

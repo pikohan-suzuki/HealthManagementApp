9c0c4d77ef7aeaa8cceee7807d80533b6a8f5b33
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-11 23:11:36 +0900

    Dialogの表示機能を追加

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/FoodStaff/addEditFoodStuff/AddEditFoodStuffFragment.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/FoodStaff/addEditFoodStuff/AddEditFoodStuffFragment.kt
@@ -4,13 +4,10 @@ import android.app.AlertDialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.*
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
-import com.amebaownd.pikohan_nwiatori.healthmanagementapp.EventObserver
+import com.amebaownd.pikohan_nwiatori.healthmanagementapp.util.EventObserver
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.R
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.databinding.FragmentAddEditFoodstuffBinding
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.util.getViewModelFactory
@@ -49,18 +46,19 @@ class AddEditFoodStuffFragment : Fragment() {
     }
 
     private fun showDialog() {
-        foodStuffViewModel.dialogOpenEvent.observe(this,EventObserver{
-            if(it) {
-                val alertDialog = AlertDialog.Builder(this.context)
-                    .setItems(
-                        resources.getStringArray(R.array.food_group),
-                        dialogItemClickListener()
-                    )
-                    .setOnCancelListener(dialogCancelListener())
-                    .setOnDismissListener(dialogDismissListener())
-                alertDialog.show()
-            }
-        })
+        foodStuffViewModel.dialogOpenEvent.observe(this,
+            EventObserver {
+                if (it) {
+                    val alertDialog = AlertDialog.Builder(this.context)
+                        .setItems(
+                            resources.getStringArray(R.array.food_group),
+                            dialogItemClickListener()
+                        )
+                        .setOnCancelListener(dialogCancelListener())
+                        .setOnDismissListener(dialogDismissListener())
+                    alertDialog.show()
+                }
+            })
 
     }
     private fun dialogItemClickListener() =

a2aeffac793887a1c3cd908a46195abdee07bc75
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-11 23:07:56 +0900

    fragment_extension

--- app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/util/FragmentExtension.kt
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/util/FragmentExtension.kt
@@ -1,16 +1,15 @@
 package com.amebaownd.pikohan_nwiatori.healthmanagementapp.util
 
 import androidx.fragment.app.Fragment
-import com.amebaownd.pikohan_nwiatori.healthmanagementapp.ServiceLoader
 import com.amebaownd.pikohan_nwiatori.healthmanagementapp.ViewModelFactory
 
 fun Fragment.getViewModelFactory(): ViewModelFactory {
 //    val app = requireContext().applicationContext
 //    val health = app as HealthManagementApp
-//    val repository = health.repository
-    val repository =
-        ServiceLoader.provideFoodStuffRepository(
-            requireContext()
-        )
-    return ViewModelFactory((repository))
+//    val foodStuffRepository = health.foodStuffRepository
+    val foodStuffRepository =
+        ServiceLoader.provideFoodStuffRepository(requireContext())
+    val dishRepository=
+        ServiceLoader.provideDishRepository(requireContext())
+    return ViewModelFactory(foodStuffRepository,dishRepository)
 }

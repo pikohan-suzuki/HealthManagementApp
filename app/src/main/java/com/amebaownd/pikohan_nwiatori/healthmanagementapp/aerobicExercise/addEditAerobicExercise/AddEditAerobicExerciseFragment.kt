69f89999c0a47de919f76bea2f91b9a265204f2e
suzuki.s <b1702996@planet.kanazawa-it.ac.jp>
2019-10-20 22:04:07 +0900

    addEditFragment

---
+++ app/src/main/java/com/amebaownd/pikohan_nwiatori/healthmanagementapp/aerobicExercise/addEditAerobicExercise/AddEditAerobicExerciseFragment.kt
@@ -0,0 +1,7 @@
+package com.amebaownd.pikohan_nwiatori.healthmanagementapp.aerobicExercise.addEditAerobicExercise
+
+import androidx.fragment.app.Fragment
+
+class AddEditAerobicExerciseFragment :Fragment(){
+
+}
